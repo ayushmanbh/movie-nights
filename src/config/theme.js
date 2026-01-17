334ac97144f9e5e5284187c36293bb3883902c9f
ayushmanbh <ashbh.dhp14@gmail.com>
2026-01-17 21:00:55 +0530

    setting off christmas theme

--- src/config/theme.js
+++ src/config/theme.js
@@ -4,5 +4,5 @@
 export const themeConfig = {
     // Set to true to enable Christmas theme (snowfall, festive colors, holiday text)
     // Set to false to use the default theme
-    enableChristmasTheme: true,
+    enableChristmasTheme: false,
 };
